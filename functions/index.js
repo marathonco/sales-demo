f76d5af41a54035869b11dff42757acb29a1e1d0
marathonco <45633205+marathonco@users.noreply.github.com>
2021-08-01 16:03:18 -0600

    functions

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,20 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
 // console.log(functions);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
-exports.accountCreate = functions.auth.user().onCreate(user => {
-    console.log(user.data);
-    userDoc = {'email' = user.data.email,
-               'displayName' = user.data.displayName}
-    admin.firestore().collection('users').doc(user.data.uid)
-    .set(userDoc).then(writeResult => {
-        console.log('User Created result:', writeResult);
+exports.accountCreate = functions.auth.user().onCreate((user) => {
+  console.log(user.data);
+  const userDoc = {"email": user.data.email,
+    "displayName": user.data.displayName};
+  admin.firestore().collection("users").doc(user.data.uid)
+      .set(userDoc).then((writeResult) => {
+        console.log("User Created result:", writeResult);
         return;
-    }).catch(err => {
-       console.log(err);
-       return;
-    });
+      }).catch((err) => {
+        console.log(err);
+        return;
+      });
 });
