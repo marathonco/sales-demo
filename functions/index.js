bebb164ad4c0708c8d524b19464f90137fd7508f
marathonco <45633205+marathonco@users.noreply.github.com>
2021-08-02 11:37:06 -0600

    userCreate and userDelete functions

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,21 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+admin.initializeApp();
 // console.log(functions);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 
-exports.accountCreate = functions.auth.user().onCreate((user) => {
-  console.log(user.data);
-  const userDoc = {"email": user.data.email,
-    "displayName": user.data.displayName};
-  admin.firestore().collection("users").doc(user.data.uid)
-      .set(userDoc).then((writeResult) => {
-        console.log("User Created result:", writeResult);
-        return;
-      }).catch((err) => {
-        console.log(err);
-        return;
-      });
+exports.userCreate = functions.auth.user().onCreate((user) => {
+  return admin
+      .firestore()
+      .collection("users")
+      .doc(user.uid)
+      .set(JSON.parse(JSON.stringify(user)));
 });
+
+exports.userDelete = functions.firestore
+    .document("users/{uid}")
+    .onDelete((snapshot, context) => {
+      return admin.auth().deleteUser(context.params.uid);
+    });
